d7344ebfd4e4b221e5003cf87d46b01fc9986afd
Elias Jaber <40572770+shafinsan@users.noreply.github.com>
2024-09-12 18:35:16 +0600

    Add files via upload

---
+++ While loop/While loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Enter Your Name :");
+            name = Console.ReadLine();
+            while (name == "")
+            {
+                Console.Write("Enter Your Name :");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine("Your name is :" + name);
+        }
+    }
+}
